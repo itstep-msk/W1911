4eaf77bdef52aa0aec41ee0f5eb9c3d3600cda68
space2pacman <space2pacman@gmail.com>
2019-11-01 01:53:33 +0300

    add SHOP

---
+++ APP/SHOP/goods.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+Goods
+
+goods
+get()
+
+*/
+
+class Goods {
+	function __construct($goods) {
+		$this->goods = $goods;
+		$this->maxGoods = count($this->goods);
+	}
+
+	function get($from, $to) {
+		$result = [];
+
+		if($to >= $this->maxGoods - 1) {
+			$to = $this->maxGoods - 1;
+		}
+
+		for($i = $from; $i <= $to; $i++) {
+			array_push($result, $this->goods[$i]);
+		}
+
+		return json_encode($result);
+	}
+}
+
+$from = $_GET["from"];
+$to = $_GET["to"];
+$file = file_get_contents("database.json");
+$db = json_decode($file, true);
+$goods = new Goods($db["goods"]);
+
+echo $goods->get($from, $to);
+
+?>
